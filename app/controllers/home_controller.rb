4f30496535b1d8d8e2eba86e8a055a0d16f03205
Edison Garcia Chinas <edisga@microsoft.com>
2020-06-20 07:50:23 -0500

    Adding method

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,53 @@
+require 'stackprof'
+
+class HomeController < ApplicationController
+
+  def write_to_file(id)
+      open('output.out', 'a') do | file |
+          file.puts "RequestId #{ id } from report at #{ Time.new } \n" 
+      end
+  end
+  
+  def custom_method()
+
+      reportnum = 1
+      i = 1
+      num = 21474
+  
+      begin
+          reportnum = reportnum * i;      
+          #Open file
+          write_to_file(i)
+          i +=1;
+       end until i > num
+
+  end
+
+  def fib(n)
+    if (n<=2)
+      return 1
+    else
+      return (fib(n-1)+fib(n-2))
+    end
+  end
+  
+  
+  def index
+
+    start_time = Time.now
+
+    StackProf.run(mode: :cpu, out: 'tmp/stackprof-cpu-myapp.dump', raw:true) do
+      p "Starting Profiler"
+      p fibonacci(40)
+      custom_method()
+      p "Finish method and profiler"
+    end
+
+    end_time = Time.now 
+    
+    elapsed_seconds = end_time.to_f - start_time.to_f
+
+    @output = "Response back from method in #{elapsed_seconds}"
+
+  end
+end
